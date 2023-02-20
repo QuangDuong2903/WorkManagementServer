2ffd93f729f3ab45b7227fe955238847f494e252
Quang Duong <quangduongptsc@gmail.com>
2023-02-20 11:05:27 +0700

    test git ignore

--- src/main/java/com/workmanagement/Application.java
+++ src/main/java/com/workmanagement/Application.java
@@ -8,7 +8,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableScheduling
 public class Application {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
 }
